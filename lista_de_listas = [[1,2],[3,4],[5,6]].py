ad9c57cb546be10849319bdffde4338b12ed2acc
MATHEUSx75 <matheus.velasco@epn.edu.ec>
2023-07-07 22:38:12 -0500

    Summit python exer

---
+++ lista_de_listas = [[1,2],[3,4],[5,6]].py
@@ -0,0 +1,5 @@
+lista_de_listas = [[1,2],[3,4],[5,6]]
+for lista in lista_de_listas :
+    for elemento in lista :
+        print (elemento)                                                                          
+
